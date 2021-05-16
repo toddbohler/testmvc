d073dea0a270300627a9dc32e21269d56f33761b
toddbohler <toddbohler@outlook.com>
2021-05-16 13:39:42 -0400

    Changes to code and client

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -22,12 +23,38 @@ namespace testmvc.Pages
         [BindProperty]
         [DisplayName("Age")]
         public int Age { get; set; }
+
         [BindProperty]
         [DisplayName("First Name")]
+        [Required(ErrorMessage = "First Name is required.")]
         public string firstName { get; set; }
+
         [BindProperty]
         [DisplayName("Last Name")]
         public string lastName { get; set; }
+
+        [BindProperty]
+        [DisplayName("Address")]
+        public string address { get; set; }
+
+        [BindProperty]
+        [DisplayName("City")]
+        public string city { get; set; }
+
+        [BindProperty]
+        [DisplayName("State")]
+        public string state { get; set; }
+
+        [BindProperty]
+        [DisplayName("Zip Code")]
+        public string zipcode { get; set; }
+
+        [BindProperty]
+        [RegularExpression("^[_A-Za-z'`+-.]+([_A-Za-z0-9'+-.]+)*@([A-Za-z0-9-])+(\\.[A-Za-z0-9]+)*(\\.([A-Za-z]*){3,})$",ErrorMessage="Invalid Email Address.")]
+        [DisplayName("EMail")]
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress(ErrorMessage="Invalid Email Address.")]
+        public string email { get; set; }
         
         public string Message { get; set; } = "Initial Request";
         private readonly ILogger<IndexModel> _logger;
@@ -55,6 +82,18 @@ namespace testmvc.Pages
             _logger.LogInformation("OnPostView started!!");
             _logger.LogInformation(Age.ToString());
             this.Message = $"OnPostView fired for {id}";
+        }
+
+        public void OnPostFirstSubmit()
+        {
+            _logger.LogInformation("FirstSubmit Triggered!!");
+            this.Message = "FirstSubmit Triggered!!";
+        }
+
+        public void OnPostSecondSubmit()
+        {
+            _logger.LogInformation("SecondSubmit Triggered!!");
+            this.Message = "SecondSubmit Triggered!!";
         }
     }
 }
