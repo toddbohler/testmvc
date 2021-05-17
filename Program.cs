51090f4b4cd6b1e96de1591cb9c3056eaac40f70
toddbohler <toddbohler@outlook.com>
2021-05-17 10:53:12 -0400

    Added Serilog to project.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace testmvc
 {
@@ -13,11 +14,31 @@ namespace testmvc
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/logs.txt", rollingInterval:RollingInterval.Month)
+                .CreateLogger();
+
+                try 
+                {
+                    Log.Information("Starting Web Host");
+                     CreateHostBuilder(args).Build().Run();
+                }
+                catch(Exception ex)
+                {
+                    Log.Fatal(ex, "Host Terminated Unexpedtedly");
+                }
+                finally
+                {
+                    Log.CloseAndFlush();
+                }
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
